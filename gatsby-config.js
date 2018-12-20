5da26c8c2c323d158697b88ffd4795bb6f2798ed
Luan Orlandi <orlandi.luan@gmail.com>
2018-12-19 22:23:33 -0200

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,14 @@ module.exports = {
     },
     'gatsby-plugin-offline',
     'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_LUAN_ORLANDI,
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
   ],
 };
